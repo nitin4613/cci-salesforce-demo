ded719d091f36258541964c4c5bd1553fd38f2a7
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-10-13 12:07:26 -0500

    testing pipeline

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,5 +1,6 @@
 echo "Installing JDK"
-sudo apt-get install openjdk-8-jdk
+sudo apt-get update
+sudo apt-get install default-jdk
 
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
