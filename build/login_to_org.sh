10dc698b67590170e660e21386052f22811c7e28
Tsvi Zandany <tsvi.zandany@circleci.com>
2022-10-14 16:25:28 -0500

    testing pipeline

--- build/login_to_org.sh
+++ build/login_to_org.sh
@@ -1,7 +1,5 @@
 echo "Logging into Salesforce Org"
 mkdir keys
-echo "-----BEGIN RSA PRIVATE KEY-----" > keys/server.key
 echo $CERT_KEY >> keys/server.key
-echo "-----END RSA PRIVATE KEY-----" >> keys/server.key
 echo "Authenticating org"
 sfdx force:auth:jwt:grant --clientid $APP_KEY --jwtkeyfile keys/server.key --username $SF_USERNAME --setdefaultdevhubusername -a DevHub
